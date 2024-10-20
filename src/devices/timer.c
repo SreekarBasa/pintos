11db4e8aed4c801e38f1b1049e92c567a018a645
SreekarBasa <sreekar.basa2004@gmail.com>
2024-10-20 22:34:52 +0530

    task2&3

--- src/devices/timer.c
+++ src/devices/timer.c
@@ -19,7 +19,18 @@ struct sleeping_thread{ //structure for a sleeping thread, pointer to a thread
   // doubly-linked list
   // for ordered lists this is useful
 };
-  
+
+struct lock sleeper_lock; //lock for sleeping_threads_list
+
+void init_sleeper_lock(void);
+bool compare_wake_up_time(const struct list_elem *a, const struct list_elem *b, void *aux UNUSED);
+void timer_wakeup(void);
+
+void
+init_sleeper_lock(void){ //function for initialization
+  lock_init(&sleeper_lock); //lock initialization
+}
+
 /* See [8254] for hardware details of the 8254 timer chip. */
 
 #if TIMER_FREQ < 19
@@ -99,7 +110,7 @@ timer_elapsed (int64_t then)
 
 
 bool
-compare_wake_up_time(const struct list_elem *a, const struct list_elem *b, void *aux UNUSED)
+compare_wake_up_time(const struct list_elem *a, const struct list_elem *b, void *aux UNUSED) //aux?
 {
   struct sleeping_thread *st_a = list_entry(a, struct sleeping_thread, elem);
   struct sleeping_thread *st_b = list_entry(b, struct sleeping_thread, elem);
@@ -117,21 +128,53 @@ timer_sleep (int64_t ticks) //argument - ticks.. no.of ticks needed to wait
   }
 
   int64_t start = timer_ticks (); //current time since sys started
-  int64_t wake_up_time = start+ticks; //added
+  int64_t wake_up_time = start + ticks; //added
+
+  lock_acquire(&sleeper_lock); //acquire the lock --task 3
 
   //sleeping thread
-  struct sleeping_thread st;
-  st.t = thread_current(); // to get current thread
-  st.wake_up_time = wake_up_time; //set wake up time
+  struct sleeping_thread *st = malloc(sizeof(struct sleeping_thread));
+  if(st==NULL){
+    PANIC("allocation problem"); //for debugging
+  }
+  st->t = thread_current(); // to get current thread
+  st->wake_up_time = wake_up_time; //set wake up time
 
-  enum intr_level old_level = intr_disable(); // to avoid race contd just like in timer ticks function
+  ASSERT(list_is_initialized(&sleeping_threads_list)); // to verify initialization
+
+  // enum intr_level old_level = intr_disable(); --previous method
+  // to avoid race contd just like in timer ticks function
   //now we need to insert the thread to sleeping_threads_list, need to be in sorted order of wake-up-times
   // passing the element
   //?
-  list_insert_ordered(&sleeping_threads_list, &st.elem, (list_less_func *) &compare_wake_up_time, NULL); 
 
+  if(list_empty(&sleeping_threads_list)){ //empty case
+    list_push_back(&sleeping_threads_list, &st->elem);
+  }else{ //sort if >1 elements only
+    list_insert_ordered(&sleeping_threads_list, &st->elem, compare_wake_up_time, NULL); // aux? 
+  }
   thread_block();
-  intr_set_level(old_level); //restore interrupts /states
+  // intr_set_level(old_level); --task 2 //restore interrupts /states
+  lock_release(&sleeper_lock); // release the lock -- task3
+}
+
+void
+timer_wakeup(void){ //new custom function to manage synchronization using locks
+  // acquire the lock for synchronization
+  lock_acquire(&sleeper_lock);
+
+  int64_t curr = timer_ticks(); // current ticks
+  while(!list_empty(&sleeping_threads_list)){
+    struct list_elem *e = list_front(&sleeping_threads_list);
+    struct sleeping_thread *st = list_entry(e, struct sleeping_thread, elem);
+    if(st->wake_up_time > curr){
+      break; // not ready yet
+    }
+    list_pop_front(&sleeping_threads_list); // remove ready unblocking thread instance
+    thread_unblock(st->t); //unblock the thread in sleeping thread instance
+    free(st); //freeing the memory
+  }
+  lock_release(&sleeper_lock); //release lock
 }
 
 /* Sleeps for approximately MS milliseconds.  Interrupts must be
@@ -209,23 +252,23 @@ static void
 timer_interrupt (struct intr_frame *args UNUSED)
 {
   ticks++; //global tick count ++
-  thread_tick (); //thread scheduling?
-
-  while(!list_empty(&sleeping_threads_list)){ // till all wake up
-    //elements of linked list
-    struct list_elem *e = list_front(&sleeping_threads_list); //like getting front element in list
-    //now e is a struct sleeping_thread instance pointer,
+  thread_tick (); //for thread scheduling
+  timer_wakeup();
+  // while(!list_empty(&sleeping_threads_list)){ // till all wake up
+  //   //elements of linked list
+  //   struct list_elem *e = list_front(&sleeping_threads_list); //like getting front element in list
+  //   //now e is a struct sleeping_thread instance pointer,
   
-    struct sleeping_thread *st = list_entry(e, struct sleeping_thread, elem); 
-    //get element from that e, which is a pointer
-    // elem helped to fetch the thread from the list
+  //   struct sleeping_thread *st = list_entry(e, struct sleeping_thread, elem); 
+  //   //get element from that e, which is a pointer
+  //   // elem helped to fetch the thread from the list
 
-    if(st->wake_up_time>ticks){ //not ready to wake up
-      break;
-    }
-    list_pop_front(&sleeping_threads_list); //remove the woken thread
-    thread_unblock(st->t); // t is the actual thread, passing it to unblock
-  }
+  //   if(st->wake_up_time>ticks){ //not ready to wake up
+  //     break;
+  //   }
+  //   list_pop_front(&sleeping_threads_list); //remove the woken thread
+  //   thread_unblock(st->t); // t is the actual thread, passing it to unblock
+  // }
 }
 
 /* Returns true if LOOPS iterations waits for more than one timer
